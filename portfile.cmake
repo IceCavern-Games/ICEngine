66a9e639d520c8df2fbb17cc68e9a5b3ccbb6133
Colby Terry <colbydude@voidteam.net>
2024-03-31 01:50:52 -0700

    Fix building. vcpkg_from_github.

--- portfile.cmake
+++ portfile.cmake
@@ -1,7 +1,9 @@
-vcpkg_from_git(
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    URL git@github.com/Ice-Cavern-Games/ICEngine.git
-    # REF 7482de6071d21db77a7236155da44c172a7f6c9e # commit hash (pointing to a version tag)
+    REPO Ice-Cavern-Games/ICEngine
+    REF "${VERSION}"
+    SHA512 0
+    HEAD_REF vcpkg-port
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -11,14 +13,16 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
 
-# vcpkg_cmake_config_fixup(
-#     PACKAGE_NAME "glfw3"
-#     CONFIG_PATH "lib/cmake/glfw3"
-# )
+vcpkg_cmake_config_fixup(
+    PACKAGE_NAME "icengine"
+    CONFIG_PATH "lib/cmake/glfw3"
+)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
@@ -26,4 +30,9 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 #     INSTALL "${SOURCE_PATH}/LICENSE.md"
 #     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
 #     RENAME copyright
+# )
+# configure_file(
+#     "${CMAKE_CURRENT_LIST_DIR}/usage"
+#     "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage"
+#     COPYONLY
 # )
