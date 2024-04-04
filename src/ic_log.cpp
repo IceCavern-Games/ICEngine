21a65f8ffa9a491a43a661abe9db7afb9602ba40
Colby Terry <colbydude@voidteam.net>
2024-04-03 21:59:54 -0700

    Fix format.

--- src/ic_log.cpp
+++ src/ic_log.cpp
@@ -8,7 +8,7 @@ namespace IC
 
     void Log::Init()
     {
-        spdlog::set_pattern("%^[%T] %n: $v%$");
+        spdlog::set_pattern("%^[%T] %n: %v%$");
 
         s_CoreLogger = spdlog::stdout_color_mt("IC");
         s_CoreLogger->set_level(spdlog::level::trace);
